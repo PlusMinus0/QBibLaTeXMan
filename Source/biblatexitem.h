bc7144c75ce92491eef3f7dc51e501759e8f46a6
PlusMinus <brandt.matthias+github@gmail.com>
2015-12-04 09:28:40 +0100

    Added typdefs for biblatex items and collections thereof

---
+++ Source/biblatexitem.h
@@ -0,0 +1,9 @@
+#ifndef BIBLATEXITEM_H
+#define BIBLATEXITEM_H
+
+#include <QHash>
+#include <QString>
+
+typedef QHash<QString, QString> BibLaTeXItem;
+
+#endif // BIBLATEXITEM_H
---
+++ Source/biblatexitemcollection.h
@@ -0,0 +1,13 @@
+#ifndef BIBLATEXITEMCOLLECTION
+#define BIBLATEXITEMCOLLECTION
+
+#include "biblatexitem.h"
+
+#include <QList>
+#include <QHash>
+#include <QString>
+
+typedef QList<BibLaTeXItem> BibLaTeXItemCollection;
+
+#endif // BIBLATEXITEMCOLLECTION
+
